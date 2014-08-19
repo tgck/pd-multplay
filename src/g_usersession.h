209eb8a9cc9629161b2baea7b534b263ae70a0ef
tgck <taniguchiko@gmail.com>
2014-08-20 01:11:50 +0900

    40:test

--- src/g_usersession.h
+++ src/g_usersession.h
@@ -1,18 +1,17 @@
 /** file for user session extention **/
 #include <time.h>
 
+
 static void hello() {
-	fprintf(stderr, "hello pd.");	
+	fprintf(stderr, "hello pd.");
 }
 
 // TODO: 精度向上
 static long new_session_id() {
-	return clock();
+	time_t t;
+	time(&t);
+	return t;
 }
-
-// add new session to already made editor
-//static void editor_add_new_session(){
-//}
 
 // user session
 typedef struct _usession 
@@ -22,14 +21,33 @@ typedef struct _usession
 	t_editor *us_editor;
 } t_usession;
 
+// session control for temporary debugs
+extern t_usession *usession_array_test[4];
+
 //static t_usession *usession_new(){
 static t_usession *usession_new(t_editor *x){
 	
 	t_usession *y = (t_usession *)getbytes(sizeof(*y));
 	
 	y->user_id = new_session_id();
-	y->us_canvas = NULL;
+	y->us_canvas = NULL; // LATER
 	y->us_editor = x;
 	
 	return y;
+}
+
+// alter session
+// TODO: assgin to key event
+static void alter_session_1(t_glist *x){
+	if (!x->gl_editor){
+		x->gl_editor = usession_array_test[0];
+		fprintf(stderr, "changed editor 2->1");
+	}
+} 
+
+static void alter_session_2(t_glist *x){
+	if (!x->gl_editor){
+		x->gl_editor = usession_array_test[1];
+		fprintf(stderr, "changed editor 1->2");
+	}	
 }
