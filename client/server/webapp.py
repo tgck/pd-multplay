fad41f408bf49e59f69256b88c4c8b931adbccb8
tgck <taniguchiko@gmail.com>
2016-05-04 21:56:12 +0900

    Webサーバの追加 - マルチスレッド化

--- client/server/webapp.py
+++ client/server/webapp.py
@@ -1,12 +1,41 @@
-from bottle import route, run, template
+# -*- coding: utf-8 -*- 
+from bottle import route, run
+from Queue import Queue, Empty
+import threading
+import time
+from datetime import datetime
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+button_pressed = Queue()
 
 @route('/')
 def hello():
     return "Hello World!"
 
-run(host='localhost', port=8080)
+def method():
+	while True:
+		print "hoge " + datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+		time.sleep(3)
+		# try:
+		# 	button_pressed.get_nowait()
+		# except Empty:
+		# 	pass
+		# else:
+  #           print "push recieved"
 
+
+#run(host='localhost', port=8080)
+
+if __name__ == '__main__':
+    threading.Thread(target=run, kwargs=dict(host='localhost', port=8080)).start()
+    method()
+
+# リスナプロセスを用意して、
+# そこから読めるものをブラウザに返す。
+
+# 非同期処理なのでスレッドを分けたい。
+# Web API 側と、ソケットリスニング側
+# 両者のやり取りは、キューを使う
+# http://stackoverflow.com/questions/19604648/threading-a-bottle-app
+
+
+# TODO : マルチスレッド時のプロセス終了
