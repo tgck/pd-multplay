8413a6323c779c5816b73d230aae438749451f03
tgck <taniguchiko@gmail.com>
2016-06-22 22:49:52 +0900

    色

--- sub/05_rb_publisher/test.rb
+++ sub/05_rb_publisher/test.rb
@@ -1,11 +1,7 @@
 #!/usr/bin/env ruby
+
 require "socket"
-
-# GUIから発せられたメッセージを、内容に応じて選別する 
-
-def check(msg)
-	msg.gsub(/a/, 'x')
-end
+load 'lib/colorize.rb'
 
 # ドロップ対象となる ワードを定義する。
 # 引数にメッセージを指定して検査する
@@ -13,13 +9,32 @@ def drop(msg)
   arr = ["pd key", "motion", "pd init"]
   return arr.any? {|w| msg.include?w }
 end
-		
+
+# Gui => Proxy => Pd  ...  黄
+def log_toPd(msg)
+  str = "g=>  [" + msg.gsub(/n/, '') + "]"
+  puts str.brown
+end
+# Gui <= Proxy <= Pd  ...  緑
+def log_toPrimeGUI(msg)
+  str = " <-p [" + msg.gsub(/\n/, '') + "]"
+  puts str.green
+end
+# Nth Gui => Proxy => Pd 
+def log_NthGuitoPd(msg, port, num)
+  str = "g+> [" + port + "] " + msg
+  case num % 8
+  when 0..8
+    puts port + ' ' + str.cyan
+  end
+end
+
 ### ##### ##### ##### ##### ##### ##### ###
 ### 最初に "wish pd-gui.tcl" などとして起動 ###
 ### ##### ##### ##### ##### ##### ##### ###
 
 pguiport = ARGV[0] # prime gui : 最初に起動した GUI が待ち受けしているポート
-p "Connect to [GUI] of port[" + pguiport + "] as client." 
+puts "Connect to [GUI] of port[" + pguiport + "] as client." 
 
 pguisock = TCPSocket.open("127.0.0.1", pguiport)
 
@@ -28,25 +43,26 @@ guipool = [pguisock]
 
 # Pd からの接続を待つ
 l_pdsock = TCPServer.open(8080)
-p "listening(pd) at 8080"
+puts "listening(pd) at 8080"
 
 ### ##### ##### ##### ##### ##### ##### ###
 ### 裏から "pd -guiport 8080" を叩くこと   ###
 ### ##### ##### ##### ##### ##### ##### ###
 
 pdsock = l_pdsock.accept
-p "accepted(pd)"
+puts "accepted(pd)"
 
 # pd から Proxy へのメッセージを処理する
 # pd ソケットに何かかかれたら、GUI(複数)のソケットに書く
 pd_thread = Thread.new do
 	loop do
 		while buf = pdsock.gets
-			p " <-p [" + buf.gsub(/\n/, '') + "]"
+			# p " <-p [" + buf.gsub(/\n/, '') + "]"
 
 			# 多重化された GUI に渡す
 			for s in guipool do
 				s.write(buf)
+        log_toPrimeGUI('.... ' + buf)
 			end
 		end
 	end
@@ -57,14 +73,16 @@ end
 gui_thread = Thread.new do
 	loop do
 		while buf = pguisock.gets
-			#p "g=>  [" + buf.gsub(/\n/, '') + "]"
+
 
       # メッセージのチェック
       if drop(buf) then
-        p "g=>  [" + "xxxx" + "][DROP] " + buf
+        #p "g=>  [" + "xxxx" + "][DROP] " + buf
+        log_toPd('DROP ' + buf)
         # NGワード持っているメッセージは送信しない
       else
-        p "g=>  [" + "xxxx" + "][PBL*] " + buf
+        #p "g=>  [" + "xxxx" + "][PBL*] " + buf
+        log_toPd('SEND ' + buf)
         # pd に向けて書く
         pdsock.write(buf)
       end
@@ -75,16 +93,16 @@ end
 # 二番目以降の GUI に向かうスレッド。
 lport = 18080
 server = TCPServer.open(lport)
-p "listening new GUI applicants at port 18080"
+puts "listening new GUI applicants at port 18080"
 
 while true
 
   Thread.start(server.accept) do |socket|
-  	p "new client accepted."
-  	p socket.peeraddr
+  	puts "new client accepted."
+  	puts socket.peeraddr
     peerport = socket.peeraddr[1]
   	sleep(3)
-  	p "pd-multplay! now mimicing init gui message"
+  	puts "pd-multplay! now mimicing init gui message"
   	
   	socket.write <<-EOF
 ::pdwindow::post {canvas 24ae50, owner 0
@@ -120,10 +138,11 @@ EOF
     while buffer = socket.gets  # 生成したソケットからの読み出し(このスレッドの担当するGUIで何かイベントがあったとき)
       # p socket.peeraddr
       if drop(buffer) then
-        p "g+>p [" + peerport + "][DROP] " + buffer
+        # puts "g+>p [" + peerport + "][DROP] " + buffer
+
         # NGワード持っているメッセージは送信しない
       else
-        p "g+> [" + peerport + "][PBL*] " + buffer
+        puts "g+> [" + peerport + "][PBL*] " + buffer
   		  # pd に向けて書く
       	pdsock.write(buffer)
       end
@@ -131,7 +150,7 @@ EOF
     
     guipool.pop(socket)
     socket.close
-    p "client exited." 
+    puts "client exited." 
   end
 end
 
